c3f05127c842fd0dd378c3c077409000672874cf
Thomas Csigai <thomascsigai1@gmail.com>
2025-01-17 12:27:23 +0100

    Adding Game Config header file

---
+++ include/GameConfig.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+// WINDOW SETTINGS
+
+constexpr uint16_t SCREEN_WIDTH = 800;
+constexpr uint16_t SCREEN_HEIGHT = 600;
+
+
+// GAME SPECIFIC SETTINGS
+
+constexpr uint16_t PADDLE_WIDTH = 5;
+constexpr uint16_t PADDLE_HEIGHT = 75;
+
+constexpr uint16_t BALL_SIZE = 5;
